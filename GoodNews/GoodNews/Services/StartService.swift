9c170a212d1dee76f707ff875cd6d4524df89a35
elenavorontsova <lena.voroncova26@yandex.ru>
2024-11-25 20:28:29 +0400

    delete the SceneDelegate

---
+++ GoodNews/GoodNews/Services/StartService.swift
@@ -0,0 +1,37 @@
+//
+//  StartService.swift
+//  GoodNews
+//
+//  Created by Елена Воронцова on 25.11.2024.
+//
+
+import UIKit
+
+final class StartService {
+    var window: UIWindow?
+    
+    init(window: UIWindow) {
+        self.window = window
+        configureWindow()
+    }
+    
+    func configureWindow() {
+        if let win = window {
+            let navigationController = UINavigationController(rootViewController: ViewController())
+            navigationController.navigationBar.isTranslucent = false
+            navigationController.navigationBar.prefersLargeTitles = true
+            
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .purple
+            appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            appearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+
+            win.rootViewController = navigationController
+            win.makeKeyAndVisible()
+        }
+    }
+}
