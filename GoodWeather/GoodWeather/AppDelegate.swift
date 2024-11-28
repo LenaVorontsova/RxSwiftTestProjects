afd3665c0c353242054c03c682f5a8891464ceab
elenavorontsova <lena.voroncova26@yandex.ru>
2024-11-28 08:42:59 +0400

    create the project

---
+++ GoodWeather/GoodWeather/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GoodWeather
+//
+//  Created by Елена Воронцова on 28.11.2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    weak var window: UIWindow?
+    var startService: StartService?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        self.startService = StartService(window: window)
+        
+        return true
+    }
+}
